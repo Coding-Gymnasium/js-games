7e54f0d658c3c9ccb9f69d6be27368ef312a0d12
Nico Rithner <nicorithner@gmail.com>
2022-02-02 17:38:23 -0700

    Add slider and refactor not to use x2

--- parallax-effect-js/script.js
+++ parallax-effect-js/script.js
@@ -6,18 +6,24 @@ let gameSpeed = 2;
 
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = './assets/layer-1.png';
-
 const backgroundLayer2 = new Image();
 backgroundLayer2.src = './assets/layer-2.png';
-
 const backgroundLayer3 = new Image();
 backgroundLayer3.src = './assets/layer-3.png';
-
 const backgroundLayer4 = new Image();
 backgroundLayer4.src = './assets/layer-4.png';
-
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = './assets/layer-5.png';
+
+const slider = document.getElementById('slider');
+slider.value = gameSpeed;
+const showGameSpeed = document.getElementById('showGameSpeed');
+showGameSpeed.innerHTML = gameSpeed;
+
+slider.addEventListener('change', function (e) {
+  gameSpeed = e.target.value;
+  showGameSpeed.innerHTML = gameSpeed;
+});
 
 class Layer {
   constructor(image, speedModifier) {
@@ -25,7 +31,6 @@ class Layer {
     this.y = 0;
     this.width = 2400;
     this.height = 700;
-    this.x2 = this.width;
     this.image = image;
     this.speedModifier = speedModifier;
     this.speed = gameSpeed * this.speedModifier;
@@ -34,20 +39,15 @@ class Layer {
   update() {
     this.speed = gameSpeed * this.speedModifier;
     if (this.x <= -this.width) {
-      this.x = this.width + this.x2 - this.speed;
-    }
-
-    if (this.x2 <= -this.width) {
-      this.x2 = this.width + this.x - this.speed;
+      this.x = 0;
     }
 
     this.x = Math.floor(this.x - this.speed);
-    this.x2 = Math.floor(this.x2 - this.speed);
   }
 
   draw() {
     ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-    ctx.drawImage(this.image, this.x2, this.y, this.width, this.height);
+    ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
   }
 }
 
