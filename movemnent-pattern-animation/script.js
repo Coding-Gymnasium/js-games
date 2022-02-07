029cf60514f0b3818b73f8cd484e36a83df7d199
Nico Rithner <nicorithner@gmail.com>
2022-02-06 21:07:17 -0700

    Create multiple animated objects
    Add sprites assets

--- movemnent-pattern-animation/script.js
+++ movemnent-pattern-animation/script.js
@@ -4,30 +4,37 @@ const ctx = canvas.getContext('2d');
 
 const CANVAS_WIDTH = (canvas.width = 500);
 const CANVAS_HEIGHT = (canvas.height = 1000);
+const numberOfEnemies = 100;
+const enemiesArray = [];
 
 class Enemy {
   constructor() {
-    this.x = 10;
-    this.y = 50;
+    this.x = Math.floor(Math.random() * canvas.width);
+    this.y = Math.floor(Math.random() * canvas.height);
     this.width = 100;
     this.height = 100;
+    this.speed = Math.random() * 4 - 2;
   }
-  update(){
-    this.x++;
-    this.y++;
+  update() {
+    this.x += this.speed;
+    this.y += this.speed;
   }
 
-  draw(){
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+  draw() {
+    ctx.strokeRect(this.x, this.y, this.width, this.height);
   }
 }
 
-const enemy1 = new Enemy();
+for (let i = 0; i < numberOfEnemies; i++) {
+  enemiesArray.push(new Enemy());
+}
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  enemy1.update();
-  enemy1.draw();
+  for (const enemy of enemiesArray) {
+    enemy.update();
+    enemy.draw();
+  }
   requestAnimationFrame(animate);
 }
 
