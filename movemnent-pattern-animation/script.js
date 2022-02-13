d6f725d1f11d5d559849fbe07b64353df6e296df
Nico Rithner <nicorithner@gmail.com>
2022-02-12 21:01:47 -0700

    Animate sprite and refactor

--- movemnent-pattern-animation/script.js
+++ movemnent-pattern-animation/script.js
@@ -7,27 +7,43 @@ const CANVAS_HEIGHT = (canvas.height = 1000);
 const numberOfEnemies = 100;
 const enemiesArray = [];
 
-const enemyImage = new Image();
-enemyImage.src = './assets/enemy1.png';
+let gameFrame = 0;
 
 class Enemy {
   constructor() {
+    this.image = new Image();
+    this.image.src = './assets/enemy1.png';
     this.x = Math.floor(Math.random() * canvas.width);
     this.y = Math.floor(Math.random() * canvas.height);
+    this.speed = Math.random() * 4 - 2;
     this.spriteWidth = 293;
     this.spriteHeight = 155;
     this.width = this.spriteWidth / 2.5;
     this.height = this.spriteHeight / 2.5;
-    this.speed = Math.random() * 4 - 2;
+    this.frame = 0;
+    this.flaspSpeed = Math.floor( Math.random() * 3 + 1 );
   }
   update() {
     this.x += this.speed;
     this.y += this.speed;
+    //animate sprites.
+    if (gameFrame % this.flaspSpeed === 0) {
+      this.frame > 4 ? (this.frame = 0) : this.frame++;
+    }
   }
 
   draw() {
-    ctx.strokeRect(this.x, this.y, this.width, this.height);
-    ctx.drawImage(enemyImage, 0, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+    ctx.drawImage(
+      this.image,
+      this.frame * this.spriteWidth,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
   }
 }
 
@@ -41,6 +57,7 @@ function animate() {
     enemy.update();
     enemy.draw();
   }
+  gameFrame++;
   requestAnimationFrame(animate);
 }
 
