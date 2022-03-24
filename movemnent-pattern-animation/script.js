48905e0cb4baeee416e59edcfdbc27a1f750e765
Nico Rithner <nicorithner@gmail.com>
2022-03-24 13:37:43 -0600

    Change to image three
    adjust spriteWidth and height

--- movemnent-pattern-animation/script.js
+++ movemnent-pattern-animation/script.js
@@ -12,10 +12,10 @@ let gameFrame = 0;
 class Enemy {
   constructor() {
     this.image = new Image();
-    this.image.src = './assets/enemy2.png';
+    this.image.src = './assets/enemy3.png';
     this.speed = Math.random() * 1.5 + 1;
-    this.spriteWidth = 266;
-    this.spriteHeight = 188;
+    this.spriteWidth = 218;
+    this.spriteHeight = 177;
     this.width = this.spriteWidth / 2.5;
     this.height = this.spriteHeight / 2.5;
     this.x = Math.floor(Math.random() * ( canvas.width - this.width ));
@@ -25,7 +25,7 @@ class Enemy {
     // this.angle = Math.random() * 2;
     this.angle = 0;
     this.angleSpeed = Math.random() * 0.2;
-    this.curve = Math.random() * 7;
+    this.curve = Math.random() * 3;
   }
   update() {
     this.x -= this.speed;
