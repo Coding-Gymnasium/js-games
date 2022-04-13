8723ddccd494da632686e90291b8dfe92ed515f0
Nico Rithner <nicorithner@gmail.com>
2022-04-13 15:27:21 -0600

    Add enemy and make Game variables
    available in Enemy class

--- npc-variety-js-games/script.js
+++ npc-variety-js-games/script.js
@@ -1,32 +1,56 @@
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
   const canvas = document.getElementById('canvas1');
   const ctx = canvas.getContext('2d');
   canvas.width = 500;
   canvas.height = 800;
 
   class Game {
-    constructor() {
+    constructor(ctx, width, height) {
+      this.ctx = ctx;
+      this.width = width;
+      this.height = height;
       this.enemies = [];
+      this.#addNewEnemy();
+      console.log(this.enemies);
     }
 
-    update() {}
-    draw() {}
+    update() {
+      this.enemies.forEach((object) => object.update());
+    }
+    draw() {
+      this.enemies.forEach((object) => object.draw());
+    }
 
-    #addNewEnemy() {}
+    #addNewEnemy() {
+      this.enemies.push(new Enemy(this));
+    }
   }
 
   class Enemy {
-    constructor() {}
-    update() {}
-    draw() {}
+    constructor(game) {
+      this.game = game;
+      this.x = this.game.width;
+      this.y = Math.random() * this.game.height;
+      this.width = 100;
+      this.height = 100;
+    }
+    update() {
+      this.x--;
+    }
+    draw() {
+      ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
   }
 
+  const game = new Game(ctx, canvas.width, canvas.height);
   let lastTime = 1;
 
   function animate(timeStamp) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     const deltaTime = timeStamp - lastTime;
     lastTime = timeStamp;
+    game.update();
+    game.draw();
 
     requestAnimationFrame(animate);
   }
