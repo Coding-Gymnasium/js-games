9ada46f2980d226a22ecc082d14848695be2bdbf
Nico Rithner <nicorithner@gmail.com>
2022-05-08 13:41:38 -0600

    Set Y paddle position to match mouse's

--- arcade-classic-on-canvas/tennis-game/script.js
+++ arcade-classic-on-canvas/tennis-game/script.js
@@ -5,15 +5,36 @@ let ballY = 50;
 let ballSpeedX = 10;
 let ballSpeedY = 5;
 let radius = 5;
+let paddle1Y;
+let paddle2Y;
+const paddleHeight = 100;
+
+function calculateMousePos(e) {
+  let rect = canvas.getBoundingClientRect();
+  let root = document.documentElement;
+  let mouseX = e.clientX - rect.left - root.scrollLeft;
+  let mouseY = e.clientY - rect.top - root.scrollTop;
+  return {
+    x: mouseX,
+    y: mouseY,
+  };
+}
 
 window.onload = function () {
   canvas = document.getElementById("gameCanvas");
   canvasContext = canvas.getContext("2d");
   let framesPerSecond = 30;
+  // paddle1Y = canvas.height / 2 - 50;
+  paddle2Y = canvas.height / 2 - paddleHeight / 2;
   setInterval(() => {
     moveEverything();
     drawEverything();
   }, 1000 / framesPerSecond);
+
+  canvas.addEventListener('mousemove', function (e) {
+    let mousePos = calculateMousePos(e);
+    paddle1Y = mousePos.y - paddleHeight / 2;
+  })
 };
 
 function moveEverything() {
@@ -38,7 +59,8 @@ function moveEverything() {
 function drawEverything() {
   colorRect(0, 0, canvas.width, canvas.height, "black");
   colorCircle(ballX, ballY, radius, "yellowgreen");
-  colorRect(5, canvas.height / 2 - 50, 10, 100, "white");
+  colorRect(5, paddle1Y, 10, paddleHeight, "white");
+  colorRect(canvas.width - 15, paddle2Y, 10, paddleHeight, "white");
 }
 
 function colorRect(leftX, topY, width, height, drawColor) {
@@ -49,6 +71,6 @@ function colorRect(leftX, topY, width, height, drawColor) {
 function colorCircle(leftX, topY, radius, drawColor) {
   canvasContext.fillStyle = drawColor;
   canvasContext.beginPath();
-  canvasContext.arc(leftX, topY, radius, 0, 2 * Math.PI)
+  canvasContext.arc(leftX, topY, radius, 0, 2 * Math.PI);
   canvasContext.fill();
 }
