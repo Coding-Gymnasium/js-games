4d89ea5c5b93bdec84eb1d3f6b3d69063c23f6ef
Nico Rithner <nicorithner@gmail.com>
2022-05-08 16:16:05 -0600

    Add scores. Need resizing

--- arcade-classic-on-canvas/tennis-game/script.js
+++ arcade-classic-on-canvas/tennis-game/script.js
@@ -9,6 +9,8 @@ let paddle1Y;
 let paddle2Y;
 const paddleHeight = 100;
 const paddleThickness = 10;
+let player1Score = 0;
+let player2Score = 0;
 
 function calculateMousePos(e) {
   let rect = canvas.getBoundingClientRect();
@@ -46,12 +48,12 @@ function ballReset() {
 }
 
 function computerMovement() {
-  let paddle2YCenter = paddle2Y + (paddleHeight/2);
-  if(paddle2YCenter < ballY - 35) {
+  let paddle2YCenter = paddle2Y + paddleHeight / 2;
+  if (paddle2YCenter < ballY - 35) {
     paddle2Y += 6;
-  } else if (paddle2YCenter > ballY + 35){
+  } else if (paddle2YCenter > ballY + 35) {
     paddle2Y -= 6;
-  } 
+  }
 }
 
 function moveEverything() {
@@ -60,15 +62,28 @@ function moveEverything() {
   ballX += ballSpeedX;
   ballY += ballSpeedY;
 
-  if (ballX < 0 || ballX > canvas.width) {
+  if (ballX > canvas.width) {
+    player1Score++;
     ballReset();
   }
+  if (ballX < 0) {
+    ballReset();
+    player2Score++;
+  }
 
-  if (ballX < paddleThickness + 5 && ballY > paddle1Y && ballY < paddle1Y + paddleHeight) {
+  if (
+    ballX < paddleThickness + 5 &&
+    ballY > paddle1Y &&
+    ballY < paddle1Y + paddleHeight
+  ) {
     ballSpeedX = -1 * ballSpeedX;
   }
 
-  if (ballX > canvas.width - ( paddleThickness + 5 ) && ballY > paddle2Y && ballY < paddle2Y + paddleHeight) {
+  if (
+    ballX > canvas.width - (paddleThickness + 5) &&
+    ballY > paddle2Y &&
+    ballY < paddle2Y + paddleHeight
+  ) {
     ballSpeedX = -1 * ballSpeedX;
   }
 
@@ -85,7 +100,15 @@ function drawEverything() {
   colorRect(0, 0, canvas.width, canvas.height, "black");
   colorCircle(ballX, ballY, radius, "yellowgreen");
   colorRect(5, paddle1Y, paddleThickness, paddleHeight, "white");
-  colorRect(canvas.width - 15, paddle2Y, paddleThickness, paddleHeight, "white");
+  colorRect(
+    canvas.width - 15,
+    paddle2Y,
+    paddleThickness,
+    paddleHeight,
+    "white"
+  );
+  canvasContext.fillText(player1Score, 100, 100);
+  canvasContext.fillText(player2Score, canvas.width - 100, 100);
 }
 
 function colorRect(leftX, topY, width, height, drawColor) {
