8954417b7ad63a5e4c078938c3299c1f508f0414
Nico Rithner <nicorithner@gmail.com>
2022-05-08 11:18:40 -0600

    Get the ball bouncing in all axis

--- arcade-classic-on-canvas/tennis-game/script.js
+++ arcade-classic-on-canvas/tennis-game/script.js
@@ -1,7 +1,9 @@
 let canvas;
 let canvasContext;
 let ballX = 50;
-let ballSpeedX = 5;
+let ballY = 50;
+let ballSpeedX = 10;
+let ballSpeedY = 5;
 let radius = 5;
 
 window.onload = function () {
@@ -16,17 +18,26 @@ window.onload = function () {
 
 function moveEverything() {
   ballX = ballX + ballSpeedX;
+  ballY = ballY + ballSpeedY;
+
   if (ballX > canvas.width) {
-    ballSpeedX = -ballSpeedX;
+    ballSpeedX = -1 * ballSpeedX;
   }
   if (ballX < 0) {
     ballSpeedX = -1 * ballSpeedX;
+  }
+
+  if (ballY > canvas.height) {
+    ballSpeedY = -1 * ballSpeedY;
+  }
+  if (ballY < 0) {
+    ballSpeedY = -1 * ballSpeedY;
   }
 }
 
 function drawEverything() {
   colorRect(0, 0, canvas.width, canvas.height, "black");
-  colorCircle(ballX, 100, "yellowgreen", radius);
+  colorCircle(ballX, ballY, radius, "yellowgreen");
   colorRect(5, canvas.height / 2 - 50, 10, 100, "white");
 }
 
@@ -35,9 +46,9 @@ function colorRect(leftX, topY, width, height, drawColor) {
   canvasContext.fillRect(leftX, topY, width, height);
 }
 
-function colorCircle(leftX, topY, drawColor, radius) {
-  canvasContext.beginPath();
+function colorCircle(leftX, topY, radius, drawColor) {
   canvasContext.fillStyle = drawColor;
-  canvasContext.arc(leftX, topY, radius, 0, 2 * Math.PI);
+  canvasContext.beginPath();
+  canvasContext.arc(leftX, topY, radius, 0, 2 * Math.PI)
   canvasContext.fill();
 }
